ca386313c084fa1fa8f5dbd3b0c256c1e937911a
Joel Vargas <joelvargasapolinario@gmail.com>
2022-09-09 11:01:26 -0400

    header icons and procfile

--- src/component/PhoneContainer.jsx
+++ src/component/PhoneContainer.jsx
@@ -4,37 +4,40 @@ const PhoneContainer = ({ data }) => {
   let dt = new Date(data.created_at);
   let hour = moment(dt).format("hh a");
   let date = moment(dt).format("MMMM, D YYYY");
-  console.log(date);
+  const count = 0;
   return (
     <div>
-      <div class="divider line one-line">{date}</div>
-
-      <div className="phone-container">
-        <div className="activity-container transform-shadow">
-          <div className="flex-row">
-            <span>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                className="bi bi-telephone-inbound-fill"
-                viewBox="0 0 16 16"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511zM15.854.146a.5.5 0 0 1 0 .708L11.707 5H14.5a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5v-4a.5.5 0 0 1 1 0v2.793L15.146.146a.5.5 0 0 1 .708 0z"
-                />
-              </svg>
-            </span>
-            <div>
-              <div className="phone-number">{data.from}</div>
-              <div className="call-detail">{`Tried to call on ${data.to}`}</div>
+      {!data.is_archived && (
+        <div>
+          <div class="divider line one-line">{date}</div>
+          <div className="phone-container">
+            <div className="activity-container transform-shadow">
+              <div className="flex-row">
+                <span>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    className="bi bi-telephone-inbound-fill"
+                    viewBox="0 0 16 16"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511zM15.854.146a.5.5 0 0 1 0 .708L11.707 5H14.5a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5v-4a.5.5 0 0 1 1 0v2.793L15.146.146a.5.5 0 0 1 .708 0z"
+                    />
+                  </svg>
+                </span>
+                <div>
+                  <div className="phone-number">{data.from}</div>
+                  <div className="call-detail">{`Tried to call on ${data.to}`}</div>
+                </div>
+              </div>
+              <span className="general-font-family transform-uppercase">{`${hour}`}</span>
             </div>
           </div>
-          <span className="general-font-family transform-uppercase">{`${hour}`}</span>
         </div>
-      </div>
+      )}
     </div>
   );
 };
